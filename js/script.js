dfd42311f0014fcf84681dd44cf097e0f70e77b8
oken1 <oku.ken1@gmail.com>
2017-12-15 12:44:06 +0900

    query change
    queryの条件を増やして処理をシンプル化

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
-chrome.windows.getCurrent( function( win ) {
-	chrome.tabs.query( { active: true }, function( tab ) {
-		for ( var i = 0 ; i < tab.length ; i++ )
-		{
-			if ( tab[i].windowId == win.id &&tab[i].url.match( /https?:/ ) )
-			{
-				document.querySelector( '#urldisplay' ).innerText = tab[i].url;
-				document.querySelector( '#qrdisplay' ).src = 'https://chart.apis.google.com/chart?chs=320x320&cht=qr&chl=' + tab[i].url;
-			}
-		}
-	});
-} );
+chrome.tabs.query( { active: true, currentWindow: true,url: '*://*/*' }, function( tab ) {
+	if ( tab.length > 0 )
+	{
+		document.querySelector( '#urldisplay' ).innerText = tab[0].url;
+		document.querySelector( '#qrdisplay' ).src = 'https://chart.apis.google.com/chart?chs=320x320&cht=qr&chl=' + tab[0].url;
+	}
+});
